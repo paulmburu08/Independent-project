f8f0d0224f7e15197ffbd003d8b1e33e9afe6687
paulmburu08 <paulmburu08@gmail.com>
2021-02-05 12:22:49 +0300

    final changes

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+setTimeout(function(){
+    $('.loader_bg').fadeToggle();
+}, 1500);
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
